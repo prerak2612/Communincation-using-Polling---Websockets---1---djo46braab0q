b82ba3b446490f6f19d96832d95202db215b4d24
prerakarya <prerak.a23csai@nst.rishihood.edu.in>
2024-12-16 21:14:50 +0530

    final

--- index.js
+++ index.js
@@ -13,19 +13,19 @@ const messages = [];
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-app.get("/findMessages",(req,res)=>{
-  return res.status(200).json({"messages":messages})
-})
-app.post("/message",(req,res)=>{
-  const {text,user} = req.body;
-  const timeStamp = new Date().toISOString
-  if(!text || !user){
-    return res.status(400).json({ "error": "Please provide a valid input" })
-  }
-  const mes = {"user":user,"text":text,"timestamp":timeStamp}
-  messages.push(mes);
-  return res.status(200).json({"message":mes})
-})
 
+app.post("/message", (req, res) => {
+  const { text, user } = req.body;
+  if (!text || !user) {
+    return res.status(400).json({ error: "Please provide a valid input" });
+  }
+  const timeStamp = new Date().toISOString;
+  const mes = { "user": user, "text": text, "timestamp": timeStamp };
+  messages.push(mes);
+  return res.status(200).json({ message: mes });
+});
+app.get("/findMessages", (req, res) => {
+  return res.status(200).json({ messages: messages });
+});
 
 module.exports = { app, messages };
